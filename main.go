5387f536ec4a5e876e1e7a69388dbd425424bcc5
Texhnolyze47 <ichigobankai136@gmail.com>
2024-03-14 13:08:33 -0600

    add: main.go

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"comercial-api/db"
+	"comercial-api/internal/database"
 	"comercial-api/router"
+	"database/sql"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+	"net/http"
 	"os"
 )
 
@@ -11,15 +17,30 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost},
+	}))
+
 	godotenv.Load(".env")
 
-	port := os.Getenv("DB_URL")
+	dbUrl := os.Getenv("DB_URL")
 
-	if port == "" {
-		port = "3000"
+	if dbUrl == "" {
+		e.Logger.Fatal("DB_URL not found")
 	}
 
-	r, err := router.SetupRouter(e)
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		e.Logger.Fatal("Error connecting to database")
+	}
+
+	apiCfg := db.ApiConfig{
+		DB: database.New(conn),
+	}
+
+	r, err := router.SetupRouter(apiCfg, e)
 
 	if err != nil {
 		e.Logger.Fatal("Error setting up router")
